66cd9b249498684901dcb9a12e4e5faf0da1e08a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-06-02 20:52:08 -0500

    Working rapper API

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = 8000;
+
+app.use(cors());
+
+const rappers =  {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Seyaah',
+        'birthLocation': 'England, London'
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancellor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'unknown': {
+        'age': 69,
+        'birthName': 'Uknown Uknowerson',
+        'birthLocation': 'Unknownsville, Unmurica'
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/api/:name', (req, res) => {
+    const rappersName = req.params.name.toLowerCase();
+    if(rappers[rappersName]) {
+        res.json(rappers[rappersName]);
+    }else {
+        res.json(rappers['unknown']);
+    }
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Listening on port number ${PORT}`);
+}) 
